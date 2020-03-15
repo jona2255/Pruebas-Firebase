7f0070d8a5c8682b161daa7c64540b36be84636d
Jona <jona2255@gmail.com>
2020-03-15 21:29:53 +0100

    Auth -> falta default_web_client_id en SignIn y SignOut

--- app/src/main/java/com/example/pruebas_firebase/HomeFragment.java
+++ app/src/main/java/com/example/pruebas_firebase/HomeFragment.java
@@ -7,10 +7,19 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 
 
 /**
@@ -73,5 +82,52 @@ public class HomeFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         navController = Navigation.findNavController(view);  // <-----------------
+
+        view.findViewById(R.id.gotoNewPostFragmentButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.navigate(R.id.newPostFragment);
+            }
+        });
+        RecyclerView postsRecyclerView = view.findViewById(R.id.postsRecyclerView);
+
+        Query query = FirebaseFirestore.getInstance().collection("posts").limit(50);
+
+        FirestoreRecyclerOptions<Post> options = new FirestoreRecyclerOptions.Builder<Post>()
+                .setQuery(query, Post.class)
+                .setLifecycleOwner(this)
+                .build();
+
+        postsRecyclerView.setAdapter(new PostsAdapter(options));
+    }
+
+    class PostsAdapter extends FirestoreRecyclerAdapter<Post, PostsAdapter.PostViewHolder> {
+        public PostsAdapter(@NonNull FirestoreRecyclerOptions<Post> options) {super(options);}
+
+        @NonNull
+        @Override
+        public PostViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return new PostViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.viewholder_post, parent, false));
+        }
+
+        @Override
+        protected void onBindViewHolder(@NonNull PostViewHolder holder, int position, @NonNull final Post post) {
+            Glide.with(getContext()).load(post.authorPhotoUrl).circleCrop().into(holder.authorPhotoImageView);
+            holder.authorTextView.setText(post.author);
+            holder.contentTextView.setText(post.content);
+        }
+
+        class PostViewHolder extends RecyclerView.ViewHolder {
+            ImageView authorPhotoImageView;
+            TextView authorTextView, contentTextView;
+
+            PostViewHolder(@NonNull View itemView) {
+                super(itemView);
+
+                authorPhotoImageView = itemView.findViewById(R.id.authorPhotoImageView);
+                authorTextView = itemView.findViewById(R.id.authorTextView);
+                contentTextView = itemView.findViewById(R.id.contentTextView);
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/pruebas_firebase/Post.java
@@ -0,0 +1,20 @@
+package com.example.pruebas_firebase;
+
+public class Post {
+    public String uid;
+    public String author;
+    public String authorPhotoUrl;
+    public String content;
+
+    // Constructor vacio requerido por Firestore
+    public Post() {}
+
+    public Post(String uid, String author, String authorPhotoUrl, String content) {
+        this.uid = uid;
+        this.author = author;
+        this.authorPhotoUrl = authorPhotoUrl;
+        this.content = content;
+    }
+
+
+}
